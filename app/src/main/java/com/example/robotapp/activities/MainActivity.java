4bdb3f467d541d280b7baf7a3de8dd5efb80b772
MarekD97 <mdorosz2@wp.pl>
2020-05-09 22:30:51 +0200

    Add BluetoothServerSocket, message handler

--- app/src/main/java/com/example/robotapp/activities/MainActivity.java
+++ app/src/main/java/com/example/robotapp/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.robotapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
@@ -11,6 +12,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_ADDRESS = "device_address";
     private String address = null;
     private BluetoothService bluetoothService;
+    BluetoothDevice bluetoothDevice;
     private byte[] buffer;
 
     @Override
@@ -37,19 +42,80 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         address = intent.getStringExtra(EXTRA_ADDRESS);
 
-        TextView textView = findViewById(R.id.textView);
+        final TextView textView = findViewById(R.id.textView);
         textView.setText(address);
-
+        bluetoothDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(address);
         bluetoothService = new BluetoothService(this, handler);
-        bluetoothService.connect(BluetoothAdapter.getDefaultAdapter().getRemoteDevice(address));
+        bluetoothService.connect(bluetoothDevice);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+                bluetoothService.send((progress + "\n").getBytes());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bluetoothService.stop();
+    }
+
+    public void onClickSend(View view) {
+        EditText textInput = findViewById(R.id.editText);
+        String inputString = textInput.getText().toString();
+
+        bluetoothService.send(inputString.getBytes());
     }
 
     private final Handler handler = new Handler() {
         @Override
-        public void handleMessage(Message message) {
-            buffer = (byte[]) message.obj;
-            String readMessage = new String(buffer, 0, message.arg1 - 1);
-            Toast.makeText(MainActivity.this, readMessage, Toast.LENGTH_LONG).show();
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothService.MessageConstants.MESSAGE_STATE_CHANGE:
+                    ProgressBar progressBar = findViewById(R.id.progressBar);
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+                            Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
+                            Toast.makeText(MainActivity.this, "Connecting...", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.VISIBLE);
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+                            Toast.makeText(MainActivity.this, bluetoothDevice.getName()+" is disconnected", Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                    break;
+                case BluetoothService.MessageConstants.MESSAGE_WRITE:
+                    byte[] buffer = (byte[]) msg.obj;
+                    TextView textView = findViewById(R.id.textView);
+                    textView.setText(buffer.toString());
+                    break;
+                case BluetoothService.MessageConstants.MESSAGE_READ:
+                case BluetoothService.MessageConstants.MESSAGE_DEVICE_NAME:
+                    Toast.makeText(MainActivity.this, "Connected to " + bluetoothDevice.getName(), Toast.LENGTH_LONG).show();
+                    break;
+                case BluetoothService.MessageConstants.MESSAGE_TOAST:
+                    Toast.makeText(MainActivity.this, msg.getData().getString(BluetoothService.MessageConstants.TOAST), Toast.LENGTH_LONG).show();
+                    finish();
+                    break;
+            }
         }
     };
 }
--- app/src/main/java/com/example/robotapp/services/BluetoothService.java
+++ app/src/main/java/com/example/robotapp/services/BluetoothService.java
@@ -2,8 +2,10 @@ package com.example.robotapp.services;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
@@ -14,55 +16,210 @@ import java.io.OutputStream;
 import java.util.UUID;
 
 public class BluetoothService {
-    static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    static final String NAME = "robotapp";
+    private static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final String mName = "RobotApp";
+
     public Handler handler; // handler that gets info from Bluetooth service
     private BluetoothAdapter bluetoothAdapter;
+
+    private AcceptThread acceptThread;
     private ConnectThread connectThread;
     private ConnectedThread connectedThread;
+    private int state;
+
+    // Constants that indicate the current connection state
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
 
     public BluetoothService(Context context, Handler handler) {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        state = STATE_NONE;
         this.handler = handler;
     }
 
     // Defines several constants used when transmitting messages between the
     // service and the UI.
-    private interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
+    public interface MessageConstants {
+        int MESSAGE_READ = 0;
+        int MESSAGE_WRITE = 1;
+        int MESSAGE_TOAST = 2;
+        int MESSAGE_STATE_CHANGE = 3;
+        int MESSAGE_DEVICE_NAME = 4;
 
-        // ... (Add other message types here as needed.)
+        // Key names received from the BluetoothChatService Handler
+        String DEVICE_NAME = "device_name";
+        String TOAST = "toast";
     }
 
-    public synchronized void connect(BluetoothDevice bluetoothDevice) {
-        if(connectThread!=null) {
+    public synchronized int getState() {
+        return state;
+    }
+
+    public synchronized void start() {
+        if (connectThread != null) {
             connectThread.cancel();
             connectThread = null;
         }
 
-        if(connectedThread!=null) {
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        if (acceptThread == null) {
+            acceptThread = new AcceptThread();
+            acceptThread.start();
+        }
+    }
+
+    public synchronized void connect(BluetoothDevice bluetoothDevice) {
+        if (state == STATE_CONNECTING) {
+            if (connectThread != null) {
+                connectThread.cancel();
+                connectThread = null;
+            }
+        }
+
+        if (connectedThread != null) {
             connectedThread.cancel();
             connectedThread = null;
         }
 
         connectThread = new ConnectThread(bluetoothDevice);
         connectThread.start();
+
+        Message msg = handler.obtainMessage(MessageConstants.MESSAGE_STATE_CHANGE, STATE_CONNECTING, -1);
+        msg.sendToTarget();
     }
 
-    public synchronized void connected(BluetoothSocket bluetoothSocket) {
-//        if(connectThread!=null) {
-//            connectThread.cancel();
-//            connectThread = null;
-//        }
+    public synchronized void connected(BluetoothSocket bluetoothSocket, BluetoothDevice device) {
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
 
-        if(connectedThread!=null) {
+        if (connectedThread != null) {
             connectedThread.cancel();
             connectedThread = null;
         }
+
+        if (acceptThread != null) {
+            acceptThread.cancel();
+            acceptThread = null;
+        }
         connectedThread = new ConnectedThread(bluetoothSocket);
         connectedThread.start();
+
+        Message msg = handler.obtainMessage(MessageConstants.MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(MessageConstants.DEVICE_NAME, device.getName());
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+        msg = handler.obtainMessage(MessageConstants.MESSAGE_STATE_CHANGE, STATE_CONNECTED, -1);
+        msg.sendToTarget();
+    }
+
+    public synchronized void stop() {
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        if (acceptThread != null) {
+            acceptThread.cancel();
+            acceptThread = null;
+        }
+        state = STATE_NONE;
+    }
+
+    public void send(byte[] out) {
+        ConnectedThread tempConnected;
+        synchronized (this) {
+            if (connectedThread == null) return;
+            tempConnected = connectedThread;
+        }
+        tempConnected.write(out);
+    }
+
+    private void connectionFailed() {
+        Message msg = handler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(MessageConstants.TOAST, "Unable to connect device");
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+        state = STATE_NONE;
+        BluetoothService.this.start();
+    }
+
+    private void connectionLost() {
+        Message msg = handler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(MessageConstants.TOAST, "Device connection was lost");
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+        state = STATE_NONE;
+
+        BluetoothService.this.start();
+    }
+
+    private class AcceptThread extends Thread {
+        //the local server socket
+        private final BluetoothServerSocket serverSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tempSocket = null;
+            try {
+                tempSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(mName, mUUID);
+            } catch (IOException e) {
+            }
+            serverSocket = tempSocket;
+            state = STATE_LISTEN;
+
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+
+            while (state != STATE_CONNECTED) {
+                try {
+                    socket = serverSocket.accept();
+                } catch (IOException e) {
+                }
+                if (socket != null) {
+                    synchronized (BluetoothService.this) {
+                        switch (state) {
+                            case STATE_LISTEN:
+                            case STATE_CONNECTING:
+                                // Situation normal. Start the connected thread.
+                                connected(socket, socket.getRemoteDevice());
+                                break;
+                            case STATE_NONE:
+                            case STATE_CONNECTED:
+                                // Either not ready or already connected. Terminate new socket.
+                                try {
+                                    socket.close();
+                                } catch (IOException e) {
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+            }
+        }
     }
 
     private class ConnectThread extends Thread {
@@ -75,8 +232,10 @@ public class BluetoothService {
 
             try {
                 tempSocket = device.createRfcommSocketToServiceRecord(mUUID);
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
             bluetoothSocket = tempSocket;
+            state = STATE_CONNECTING;
         }
 
         public void run() {
@@ -84,15 +243,19 @@ public class BluetoothService {
 
             try {
                 bluetoothSocket.connect();
-                System.out.println("Connected");
             } catch (IOException connectException) {
                 // Unable to connect; close the socket and return.
                 try {
                     bluetoothSocket.close();
-                } catch (IOException closeException) {}
+                } catch (IOException closeException) {
+                }
+                connectionFailed();
                 return;
             }
-            connected(bluetoothSocket);
+            synchronized (BluetoothService.this) {
+                connectThread = null;
+            }
+            connected(bluetoothSocket, bluetoothSocket.getRemoteDevice());
         }
 
         public void cancel() {
@@ -116,13 +279,13 @@ public class BluetoothService {
 
             try {
                 tempIn = socket.getInputStream();
-            } catch (IOException e) {}
-            try {
                 tempOut = socket.getOutputStream();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
 
             inputStream = tempIn;
             outputStream = tempOut;
+            state = STATE_CONNECTED;
         }
 
         public void run() {
@@ -130,7 +293,7 @@ public class BluetoothService {
             int numBytes; // bytes returned from read()
 
             // Keep listening to the InputStream until an exception occurs.
-            while (true) {
+            while (state == STATE_CONNECTED) {
                 try {
                     // Read from the InputStream.
                     numBytes = inputStream.read(buffer);
@@ -139,6 +302,7 @@ public class BluetoothService {
                             buffer);
                     readMsg.sendToTarget();
                 } catch (IOException e) {
+                    connectionLost();
                     break;
                 }
             }
@@ -149,13 +313,15 @@ public class BluetoothService {
             try {
                 outputStream.write(bytes);
 
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
         }
 
         public void cancel() {
             try {
                 bluetoothSocket.close();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
         }
     }
 }
